7af56e9bbe2f308e94fb6c7691085bdb43b3aee4
Edir P Santos <fennder@gmail.com>
2023-05-22 17:41:58 -0300

    configuração index, topbar e footer

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,8 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+import 'bootstrap'
+import 'application.scss'
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -18,3 +20,10 @@ require("channels")
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
+
+(() => {
+    'use strict'
+    document.querySelector('#navbarSideCollapse').addEventListener('click', () => {
+      document.querySelector('.offcanvas-collapse').classList.toggle('open')
+    })
+  })()
